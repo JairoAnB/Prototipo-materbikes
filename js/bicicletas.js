564966da307d898ec8d4e407fa01339befabe191
Jairo <jairoandregonzales@gmail.com>
2024-06-24 20:52:06 -0400

    primero

---
+++ js/bicicletas.js
@@ -0,0 +1,48 @@
+/* Esta lista podría ser reemplazada por la respuesta de un backend */
+const bicicletas = [
+  {
+    id:1,
+    nombre: "Bicicleta sport red",
+    precio: 12000,
+  },
+  {
+    id:2,
+    nombre: "bicicleta Negra",
+    precio: 100000,
+  },
+  {
+    id:3,
+    nombre: "bicicleta electro sport negra",
+    precio: 400000,
+  },
+  {
+    id:4,
+    nombre: "Bicileta specialized azul marino",
+    precio: 150000,
+  },
+  {
+    id:5,
+    nombre: "Bicicleta azul blanco specialized",
+    precio: 50000,
+  },
+  {
+    id:6,
+    nombre: "Bicicleta sport pro specialized",
+    precio: 500000,
+  },
+    {
+    id:7,
+    nombre: "Bicicleta ruta/gravel disco aro 700 11 velocidades rc520",
+    precio: 500000,
+  },
+  {
+    id:8,
+    nombre: "Bicicleta ruta ncr cf tiagra azul",
+    precio: 159000,
+  },
+  {
+    id:9,
+    nombre: "Bicicleta de ruta edr cf 105 cn fr",
+    precio: 2150000,
+  }
+]
